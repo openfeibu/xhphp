d031907441f4df22eb8e57fc63a16c4176705c1a
wzjxh <wzjxh@23e8a065-ee1d-f14c-8958-478b19625c16>
2016-11-19 07:46:58 +0000

    git-svn-id: https://39.108.144.9:8443/svn/xhphp@176 23e8a065-ee1d-f14c-8958-478b19625c16

--- projects/1.0.0/app/Http/Controllers/ReportNetworkFailureController.php
+++ projects/1.0.0/app/Http/Controllers/ReportNetworkFailureController.php
@@ -19,24 +19,19 @@ class ReportNetworkFailureController extends Controller
         $this->middleware('auth');
     }
 	
-	public function network_login($account,$password,$help){
-		$rule = [
-            'token' => 'required',
-        ];
-        $help->validateParameter($rule);
-
-        $curl = curl_init();
-        $cookie = tempnam('./temp', md5($account.time()));
+	public function common($url,$postDate){
+		$curl = curl_init();
+        $cookie = tempnam('./temp', md5(time()));
 		$this->cookie = $cookie;
         curl_setopt_array($curl, array(
-            CURLOPT_URL => "http://211.66.88.6/xyw/logincheck.asp",
+            CURLOPT_URL => $url,
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_ENCODING => "",
             CURLOPT_MAXREDIRS => 10,
             CURLOPT_TIMEOUT => 10, 	
             CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
             CURLOPT_CUSTOMREQUEST => "POST",
-            CURLOPT_POSTFIELDS => "login_name=" . $account . "&login_password=" . $password,
+            CURLOPT_POSTFIELDS => $postDate,
             CURLOPT_COOKIEJAR => $this->cookie,
             CURLOPT_HTTPHEADER => array(
               	"content-type: application/x-www-form-urlencoded",
@@ -46,196 +41,132 @@ class ReportNetworkFailureController extends Controller
             CURLOPT_USERAGENT => "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
         ));
 
-        $response = curl_exec($curl);
-        $result = curl_getinfo($curl);
-		return $result;
+        return $response = curl_exec($curl);
 	}
 	
-	public function checkPassword(Request $request, HelpService $help,UserService $user){
+	public function checkPassword(Request $request, HelpService $help){
 		$rule = [
             'account' => 'required',
             'password' => 'required',
+            'token' => 'required',
         ];
         $help->validateParameter($rule);
 		
-		$result = $this->network_login($request->account,$request->password,$help);
-		if ($result['http_code'] == 302 && !empty($result['redirect_url'])) {
-			$uid = $user->getUser()->uid;
-			$networkReport = NetworkReport::where('uid',$uid)
-											->orderBy('id','desc')
-											->first();
-			if($networkReport){
-				$networkReport->account = $request->account;
-				$networkReport->password = \Crypt::encrypt($request->password);
-				$networkReport->ip = $request->ip();
-				$networkReport->save();
-			}else{
-				$nr = new NetworkReport;
-				$nr->uid = $uid;
-				$nr->account = $request->account;
-				$nr->password = \Crypt::encrypt($request->password);
-				$nr->ip = $request->ip();
-				$nr->save();
-			}
+		$url = "http://211.66.88.153/network/checkPassword.php";
+		$postDate = "account=" . $request->account . "&password=" . $request->password. "&token=" . $request->token;
+		$response = $this->common($url,$postDate);
+		
+		if($response == 404){
 			return [
-                'code' => 200,
-                'detail' => '账号密码正确',
-				'data' => $request->account
-            ];
-		}else if(empty($result['content_type']) && $result['http_code'] == 0){
+				'code' => 404,
+	            'detail' => '学号不存在',
+			];
+		}elseif($response == 400){
 			return [
-				'code' => 503,
-				'detail' => '服务器连接超时',
+				'code' => 400,
+	            'detail' => '该用户不是校园网用户',
+			];
+		}elseif($response == 100){
+			return [
+				'code' => 100,
+	            'detail' => '密码错误',
+			];
+		}elseif($response == 405){
+			return [
+				'code' => 405,
+	            'detail' => 'token无效',
+			];
+		}elseif(explode('-',$response)[0] == 200){
+			return [
+				'code' => 200,
+	            'detail' => '请求成功',
+	            'data' => explode('-',$response)[1] 
 			];
 		}else{
 			return [
-                'code' => 400,
-                'detail' => '账号或密码输入错误，请重新填写',
-            ];
+				'code' => 500,
+	            'detail' => '未知错误',
+			];
 		}
 	}
 	
-	public function checkByToken(UserService $user,HelpService $help){
+	public function checkByToken(HelpService $help,Request $request,UserService $user){
 		$rule = [
             'token' => 'required',
         ];
         $help->validateParameter($rule);
 		
-		$uid = $user->getUser()->uid;
-		$networkReport = NetworkReport::where('uid',$uid)
-										->whereNotNull('account')
-										->orderBy('id','desc')
-										->first();	
-		if($networkReport){
-			$result = $this->network_login($networkReport->account,\Crypt::decrypt($networkReport->password),$help);
-			if ($result['http_code'] === 302 and !empty($result['redirect_url'])) {
-				return [
-					'code' => 200,
-					'detail' => '账号密码正确',
-					'data' => $networkReport->account
-				];
-			}else if(empty($result['content_type']) && $result['http_code'] == 0){
-				return [
-					'code' => 503,
-					'detail' => '服务器连接超时',
-				];
-			}else{
-				return [
-					'code' => 400,
-					'detail' => '账号或密码输入错误，请重新填写',
-				];
-			}
+		$url = "http://211.66.88.153/network/loginByToken.php";
+		$postDate = "token=" . $request->token;
+		$response = $this->common($url,$postDate);
+		if($response == 200){
+			$uid = $user->getUser()->uid;
+			$networkReport = NetworkReport::where('uid',$uid)
+											->first();
+			return [
+				'code' => 200,
+	            'detail' => '请求成功',
+	            'data' => $networkReport->account
+			];
+		}elseif($response == 405){
+			return [
+				'code' => 405,
+	            'detail' => 'token无效',
+			];
+		}elseif($response == 403){
+			return [
+				'code' => 403,
+	            'detail' => '还未绑定学号',
+			];
 		}else{
 			return [
-                'code' => 401,
-                'detail' => '还未绑定学号',
-            ];
+				'code' => 500,
+	            'detail' => '未知错误',
+			];
 		}
 	}
 	
 	public function reportByToken(Request $request,HelpService $help,UserService $user){
 		$rule = [
             'miaoshu' => 'required',
-        ];
-        $help->validateParameter($rule);
-		
-		$checkByToken = $this->checkByToken($user,$help);
-		if($checkByToken['code'] == 200){
-			$curl = curl_init();
-			curl_setopt_array($curl, array(
-				CURLOPT_URL => "http://211.66.88.6/xyw/gzfw.asp?Myaction=gzdj",
-				CURLOPT_RETURNTRANSFER => true,
-				CURLOPT_ENCODING => "",
-				CURLOPT_MAXREDIRS => 10,
-				CURLOPT_TIMEOUT => 10,
-				CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-				CURLOPT_CUSTOMREQUEST => "POST",
-				CURLOPT_POSTFIELDS => "miaoshu=" . mb_convert_encoding($request->miaoshu, "gb2312", "UTF-8"). "&guzhang=1",
-				CURLOPT_COOKIEFILE => $this->cookie,
-				CURLOPT_HTTPHEADER => array(
-				  "content-type: application/x-www-form-urlencoded"
-				),
-			));
-
-			$response = curl_exec($curl);
-			$result = curl_getinfo($curl);
-			$response_utf8 = mb_convert_encoding($response, "UTF-8", "gb2312"); 
-
-			if (strstr($response_utf8, '您上次提交的故障尚未处理，请耐心等待网管员上门解决')) {
-				return [
-					'code' => 403,
-					'detail' => '您上次提交的故障尚未处理，请耐心等待网管员上门解决！',
-				];
-			}
-			
-			if (strstr($response, 'alert(')) {
-				return [
-					'code' => 404,
-					'detail' => '你尚未开通校园网',
-				];
-			}
-			if ($result['http_code'] == 200) {
-				return [
-					'code' => 200,
-					'detail' => '报修成功',
-				];
-			} else {
-				return [
-					'code' => 500,
-					'detail' => '系统升级中，请稍后再试',
-				];
-			}
-		}else if(empty($result['content_type']) && $result['http_code'] == 0){
-			return [
-				'code' => 503,
-				'detail' => '服务器连接超时',
-			];
-		}else if($checkByToken['code'] == 400){
-			return [
-				'code' => 400,
-				'detail' => '账号或密码输入错误，请重新填写',
-			];
-		}else if($checkByToken['code'] == 401){
-			return [
-                'code' => 401,
-                'detail' => '还未绑定学号 ',
-            ];
-		}		
-	}
-	
-	/* public function Unbinding(Request $request,HelpService $help,UserService $user){
-		$rule = [
             'token' => 'required',
         ];
         $help->validateParameter($rule);
 		
-		$uid = $user->getUser()->uid;
-		$networkReport = NetworkReport::where('uid',$uid)
-									->orderBy('id','desc')
-									->whereNotNull('account')
-									->where('account','<>',"")
-									->first();
+		$url = "http://211.66.88.153/network/reportByToken.php";
+		$postDate = "miaoshu=" . $request->miaoshu . "&token=" . $request->token;
+		$response = $this->common($url,$postDate);
 		
-		if($networkReport){
-			$networkReport->account = null;
-			$networkReport->password = null;
-			$networkReport->ip = $request->ip();
-			if($networkReport->save()){
-				return [
-					'code' => 200,
-					'detail' => '解绑成功',
-				];
-			}else{
-				return [
-					'code' => 403,
-					'detail' => '解绑失败，请重新尝试',
-				];
-			}
+		if($response == 404){
+			return [
+				'code' => 404,
+	            'detail' => '学号不存在',
+			];
+		}elseif($response == 400){
+			return [
+				'code' => 400,
+	            'detail' => '该用户不是校园网用户',
+			];
+		}elseif($response == 100){
+			return [
+				'code' => 100,
+	            'detail' => '密码错误',
+			];
+		}elseif($response == 405){
+			return [
+				'code' => 405,
+	            'detail' => 'token无效',
+			];
+		}elseif($response == 200){
+			return [
+				'code' => 200,
+	            'detail' => '请求成功',
+			];
 		}else{
 			return [
-                'code' => 401,
-                'detail' => '还未绑定学号',
-            ];
-		}
-	} */
+				'code' => 500,
+	            'detail' => '未知错误',
+			];
+		}	
+	}
 }
