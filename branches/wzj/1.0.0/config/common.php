1dbac207138372d2b08a64b463beee4fa1581d90
wzjxh <wzjxh@23e8a065-ee1d-f14c-8958-478b19625c16>
2016-11-19 03:31:09 +0000

    git-svn-id: https://39.108.144.9:8443/svn/xhphp@165 23e8a065-ee1d-f14c-8958-478b19625c16

--- branches/wzj/1.0.0/config/common.php
+++ branches/wzj/1.0.0/config/common.php
@@ -4,7 +4,7 @@ return [
 	'real_name_url' => 'http://gzctsmrz.ews.m.jaeapp.com/web/real-order.action',
 	'telecom_return_url' => config('app.url').'/fb/html/telecom_order.html?device=android',
 	'telecom_show_url'	=> config('app.url'),
-	'order_return_url' => config('app.url'),
+	'order_return_url' => config('app.url').'/fb/index.html#/work',
 	'order_show_url'	=> config('app.url'),
 	'img_type' => [
 		"jpg","gif","bmp","jpeg","png"
