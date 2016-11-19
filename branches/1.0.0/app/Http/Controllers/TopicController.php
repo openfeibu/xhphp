b0958e8fa1013d8cdd20d963335abda42ab798d3
wzjxh <wzjxh@23e8a065-ee1d-f14c-8958-478b19625c16>
2016-11-19 07:45:18 +0000

    git-svn-id: https://39.108.144.9:8443/svn/xhphp@175 23e8a065-ee1d-f14c-8958-478b19625c16

--- branches/1.0.0/app/Http/Controllers/TopicController.php
+++ branches/1.0.0/app/Http/Controllers/TopicController.php
@@ -310,6 +310,7 @@ class TopicController extends Controller
         ];
         //创建评论
         $id = $this->topicService->createComment2Topic($param);
+        $comments = $this->topicService->getTopicAllCommentsList($param);
 		//透传给话题者/被评论者
 		$user = $this->userService->getUser();
 		if($to_uid != $user->uid)
@@ -336,9 +337,8 @@ class TopicController extends Controller
         return [
             'code' => 200,
             'detail' => '请求成功',
-            'data' => [
-				'comment_id' => $id
-            ],
+            'comment_id' => $id,
+            'data' => $comments,
         ];
     }
 
