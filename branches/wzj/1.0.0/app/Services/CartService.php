3b8633bc1c00b1fe1c9fc3bccaebeaa5487a5681
wzjxh <wzjxh@23e8a065-ee1d-f14c-8958-478b19625c16>
2016-11-25 02:19:10 +0000

    git-svn-id: https://39.108.144.9:8443/svn/xhphp@191 23e8a065-ee1d-f14c-8958-478b19625c16

--- branches/wzj/1.0.0/app/Services/CartService.php
+++ branches/wzj/1.0.0/app/Services/CartService.php
@@ -6,6 +6,8 @@ use Log;
 use Illuminate\Http\Request;
 use App\Repositories\UserRepository;
 use App\Repositories\CartRepository;
+use App\Services\ShopService;
+use App\Services\GoodsService;
 
 class CartService
 {
@@ -17,12 +19,15 @@ class CartService
 
 	function __construct(Request $request,
 						 CartRepository $cartRepository,
-						 UserRepository $userRepository)
+						 UserRepository $userRepository,
+						 ShopService $shopService,
+						 GoodsService $goodsService)
 	{
 		$this->request = $request;
         $this->cartRepository = $cartRepository;
         $this->userRepository = $userRepository;
-
+        $this->shopService = $shopService;
+        $this->goodsService = $goodsService;
 	}
 	public function addToCart($goods,$uid)
 	{
@@ -39,10 +44,6 @@ class CartService
 	public function updateGoodsNumber ($goods_id,$goods_number,$uid)
 	{
 		return $this->cartRepository->updateGoodsNumber($goods_id,$goods_number,$uid);
-	}
-	public function getCarts($uid)
-	{
-		return  $this->cartRepository->getCarts($uid);
 	}
 	public function getShopCarts($shop_id,$uid)
 	{
@@ -77,5 +78,46 @@ class CartService
 	public function getShopCartsByCartIds ($shop_id,$cart_ids,$uid)
 	{
 		return $this->cartRepository->getShopCartsByCartIds($shop_id,$cart_ids,$uid);
+	}
+	public function getCarts ($uid)
+	{
+		$cartShop = $this->getShop($uid);
+		$total = 0;
+		$arrCarts = array();
+		foreach( $cartShop as $key => $cartShopValue )
+		{
+			$carts = $this->getShopCarts($cartShopValue->shop_id,$uid);
+			$shopDetail = $this->shopService->getShop($cartShopValue->shop_id);
+			$arrCarts[$cartShopValue->shop_id] = array(
+				'shop_name' 	=> $shopDetail->shop_name,
+				'shop_id'		=> $shopDetail->shop_id,
+				'shop_status'	=> $shopDetail->shop_status,
+				'shop_status_description' => trans("common.shop_status.$shopDetail->shop_status"),
+			);
+			$shop_total = 0; 
+			foreach( $carts as $k => $cartsValue )
+			{
+				$goodsDetail = $this->goodsService->existGoods($cartsValue->goods_id);
+				$goods_total = $cartsValue->goods_price * $cartsValue->goods_number;
+				$arrCarts[$cartShopValue->shop_id]['carts'][$cartsValue->cart_id] = array(
+					'goods_desc' 	=> $goodsDetail->goods_desc ,
+					'goods_name' 	=> $goodsDetail->goods_name,
+					'goods_img'  	=> $goodsDetail->goods_img,
+					'is_on_sale'	=> $goodsDetail->is_on_sale,
+					'goods_id'	 	=> $cartsValue->goods_id,				
+					'goods_price'	=> $cartsValue->goods_price,
+					'goods_number'	=> $cartsValue->goods_number,
+					'cart_id'		=> $cartsValue->cart_id,
+					'goods_total'	=> $goods_total,
+				);
+				$shop_total += $goods_total;
+			}
+			$arrCarts[$cartShopValue->shop_id]['shop_total'] =  $shop_total;
+			$total += $shop_total;
+		}
+		return [
+			'carts' => $arrCarts,
+			'total' => $total,
+		];
 	}
 }
