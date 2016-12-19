32e952352a19fd1949a8b4d9fe835ce6f513b5e4
wzjxh <wzjxh@23e8a065-ee1d-f14c-8958-478b19625c16>
2016-12-19 12:02:42 +0000

    git-svn-id: https://39.108.144.9:8443/svn/xhphp@214 23e8a065-ee1d-f14c-8958-478b19625c16

--- projects/1.0.0/config/common.php
+++ projects/1.0.0/config/common.php
@@ -5,6 +5,7 @@ return [
 	'telecom_return_url' => config('app.url').'/fb/html/telecom_order.html?device=android',
 	'telecom_show_url'	=> config('app.url'),
 	'order_return_url' => config('app.url').'/fb/index.html#/work',
+	'order_info_return_url' => config('app.url').'/fb/index.html',
 	'order_show_url'	=> config('app.url'),
 	'img_type' => [
 		"jpg","gif","bmp","jpeg","png"
