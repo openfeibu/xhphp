83057ae7961046061ad1c7dd69d80699ed8a8eab
wzjxh <wzjxh@23e8a065-ee1d-f14c-8958-478b19625c16>
2016-12-23 07:33:26 +0000

    git-svn-id: https://39.108.144.9:8443/svn/xhphp@237 23e8a065-ee1d-f14c-8958-478b19625c16

--- projects/1.0.0/app/Http/Controllers/OrderInfoController.php
+++ projects/1.0.0/app/Http/Controllers/OrderInfoController.php
@@ -253,6 +253,7 @@ class OrderInfoController extends Controller
         	'body' => '校汇商店订单', 
         	'total_fee' => $total_fee,
         	'trade_type' => 'Shopping',
+        	'mobile' => $this->user->mobile
         ];								
         $data = $this->payService->payHandle($request->pay_id,$pay_platform,'shop',$data);		
         return [
