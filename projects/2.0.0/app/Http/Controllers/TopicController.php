900799914612527daef42d5fcd47e04960286dbe
wzj <wzj@23e8a065-ee1d-f14c-8958-478b19625c16>
2017-03-23 07:38:49 +0000

    git-svn-id: https://39.108.144.9:8443/svn/xhphp@387 23e8a065-ee1d-f14c-8958-478b19625c16

--- projects/2.0.0/app/Http/Controllers/TopicController.php
+++ projects/2.0.0/app/Http/Controllers/TopicController.php
@@ -379,7 +379,7 @@ class TopicController extends Controller
 				'attr' => 'topic',
 			];
 			$this->notificationService->store($notificatin_data);
-			$where = ['uid' => $user->uid ,'attr' => 'topic','read'=>0];
+			$where = ['uid' => $to_uid ,'attr' => 'topic','read'=>0];
 			$count = $this->notificationService->newTopicNotificationCount($where);
 			$data = [
 				'refresh' => 1,
