175ee6b7f58d5b32348f07a1761f07d2bddf99b3
wzj <wzj@23e8a065-ee1d-f14c-8958-478b19625c16>
2017-04-11 16:25:01 +0000

    git-svn-id: https://39.108.144.9:8443/svn/xhphp@453 23e8a065-ee1d-f14c-8958-478b19625c16

--- projects/2.0.0/public/mobile/js/vue-template.js
+++ projects/2.0.0/public/mobile/js/vue-template.js
@@ -952,7 +952,9 @@ var product = Vue.extend({
               }
               $.getJSON(this.localhost+'/business/goods/getGoodses',prodata,function(data){
                 that.$indicator.close();
-                this.$refs.loadmore.onBottomLoaded();
+                console.log(this.$refs)
+                console.log(this.$refs.$loadmore)
+                // this.$refs.loadmore.onBottomLoaded();
                 if(flag){
                     that.list = [];
                     // $(".mint-loadmore-content").css({"transform":"matrix(1, 0, 0, 1, 0, 0)","-webkit-transform":"matrix(1, 0, 0, 1, 0, 0)","-o-transform":"matrix(1, 0, 0, 1, 0, 0)","-moz-transform":"matrix(1, 0, 0, 1, 0, 0)","-ms-transform":"matrix(1, 0, 0, 1, 0, 0)"})
