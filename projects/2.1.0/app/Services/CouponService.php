a8b62ad6f9656d8341ce8dd7b42d1331d1bb14eb
wzjxh <wzjxh@23e8a065-ee1d-f14c-8958-478b19625c16>
2017-05-20 15:29:29 +0000

    git-svn-id: https://39.108.144.9:8443/svn/xhphp@565 23e8a065-ee1d-f14c-8958-478b19625c16

--- projects/2.1.0/app/Services/CouponService.php
+++ projects/2.1.0/app/Services/CouponService.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Services\HelpService;
 use App\Repositories\UserRepository;
 use App\Repositories\CouponRepository;
+use App\Repositories\GameRepository;
 
 class CouponService
 {
@@ -19,12 +20,14 @@ class CouponService
 	public function __construct(Request $request,
 						 HelpService $helpService,
 						 CouponRepository $couponRepository,
+						 GameRepository $gameRepository,
 						 UserRepository $userRepository)
 	{
 		$this->request = $request;
 		$this->helpService = $helpService;
         $this->couponRepository = $couponRepository;
         $this->userRepository = $userRepository;
+		$this->gameRepository = $gameRepository;
 	}
     public function getUserCoupons($where,$type,$num =20)
     {
@@ -56,13 +59,18 @@ class CouponService
 	}
 	public function createUserRegisterCoupon($uid)
 	{
-		$this->couponRepository->createUserCoupon([
-			'uid' => $uid,
-			'overdue' => date("Y-m-d H:i:s",strtotime("+1week",time())) ,
-			'receive' => dtime(),
-			'status' => 'unused',
-			'min_price' => '10',
-			'price' => '5',
-		]);
+		$game = $this->gameRepository->getGame(['name' => 'coupon']);
+		$time = time();
+		if($game->status ==1 && $time >= strtotime($game->starttime) && $time <= strtotime($game->endtime))
+		{
+			$this->couponRepository->createUserCoupon([
+			   'uid' => $uid,
+			   'overdue' => date("Y-m-d H:i:s",strtotime("+1week",time())) ,
+			   'receive' => dtime(),
+			   'status' => 'unused',
+			   'min_price' => '10',
+			   'price' => '5',
+		   ]);
+		}
 	}
 }
