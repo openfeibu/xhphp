16948e345c48670fcee43f2d6e5bb04714d8fe86
wzjxh <wzjxh@23e8a065-ee1d-f14c-8958-478b19625c16>
2017-08-15 15:02:15 +0000

    git-svn-id: https://39.108.144.9:8443/svn/xhphp@598 23e8a065-ee1d-f14c-8958-478b19625c16

---
+++ projects/3.0.0/config/zhima.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'host' => 'http://dm-102.data.aliyun.com',
+    'certify_path' => '/rest/161225/zmxy/api/zhima.customer.certification.certify.json',
+    'initialize_path' => '/rest/161225/zmxy/api/zhima.customer.certification.initialize.json',
+    'query_path' => '/rest/161225/zmxy/api/zhima.customer.certification.query.json',
+    'appcode' => '61c92db5d36c4c119c12294fb17f4382',
+];
