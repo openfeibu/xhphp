749a2703f9a2b70a5f225e3571ac5540fa49aa7f
wzj <wzj@23e8a065-ee1d-f14c-8958-478b19625c16>
2017-09-06 17:18:17 +0000

    git-svn-id: https://39.108.144.9:8443/svn/xhphp@728 23e8a065-ee1d-f14c-8958-478b19625c16

--- projects/3.0.0/app/Helper/FunctionHelper.php
+++ projects/3.0.0/app/Helper/FunctionHelper.php
@@ -261,7 +261,40 @@ if(!function_exists('friendlyDate')){
 	        } else {
 	            return date('Y-m-d H:i', $sTime);
 	        }
-	    } elseif ($type == 'mohu') {
+	    }elseif ($type == 'weixin'){
+			//返回的时间
+	        $timeStr = "";
+	        //获取当前时间
+	        $addTime = explode(',', date('Y,n,j,w,a,h,i,y', $sTime));//年，月，日，星期，上下午，时，分
+	        $nowTime = explode(',', date('Y,n,j,w,a,h,i,y', $cTime));
+
+	        $dayPerMonthAddTime = getDayPerMonth($addTime[0]);
+	        $week = array(0=>"星期日",1=>"星期一",2=>"星期二",3=>"星期三",4=>"星期四",5=>"星期五",6=>"星期六");
+	        //如果时间差小于一天的,显示（上午 时间） / （下午 时间）
+	        if($addTime[0] == $nowTime[0] && $addTime[1] == $nowTime[1] && $addTime[2] == $nowTime[2]) {
+	            $timeStr .= $addTime[5] . ":" . $addTime[6];
+	        } else if(($addTime[0] == $nowTime[0] && $addTime[1] == $nowTime[1] && $addTime[2] == $nowTime[2]-1)
+	            || ($addTime[0] == $nowTime[0] && $nowTime[1]-$addTime[1] == 1 && $dayPerMonthAddTime[$addTime[1]] == $addTime[2] && $nowTime[2] == 1)
+	            || ($nowTime[0]-$addTime[0] == 1 && $addTime[1] == 12 && $addTime[2] == 31 && $nowTime[1] == 1 && $nowTime[2] == 1)) {
+					//如果时间差在昨天,三种情况（同一月份内跨一天、月末跨越到月初、年末跨越到年初）显示格式：昨天 时:分 上午/下午
+	            $timeStr .= "昨天 " . $addTime[5] . ":" . $addTime[6] . " ";
+	        } else if(($addTime[0] == $nowTime[0] && $addTime[1] == $nowTime[1] && $nowTime[2] - $addTime[2] < 7)
+	            || ($addTime[0] == $nowTime[0] && $nowTime[1]-$addTime[1] == 1 && $dayPerMonthAddTime[$addTime[1]]-$addTime[2]+$nowTime[2] < 7
+	                || ($nowTime[0]-$addTime[0] == 1 && $addTime[1] == 12 && $nowTime[1] == 1 && 31-$addTime[2]+$nowTime[2] < 7))) { //如果时间差在一个星期之内的,也是三种情况，显示格式：星期 时:分 上午/下午
+
+	            $timeStr .= $week[$addTime[3]] . " " . $addTime[5] . ":" . $addTime[6];
+	        } else { //显示格式：月/日/年 时:分 上午/下午
+	            $timeStr .= $addTime[1] . "/" . $addTime[2] . "/" . $addTime[7] . " " . $addTime[5] . ":" . $addTime[6];
+	        }
+
+	        if($addTime[4] == "am") {
+	            $timeStr .= " 上午";
+	        } else if($addTime[4] == "pm") {
+	            $timeStr .= " 下午";
+	        }
+
+	        return $timeStr;
+		}elseif ($type == 'mohu') {
 	        if ($dTime < 60) {
 	            return $dTime.'秒前';
 	        } elseif ($dTime < 3600) {
@@ -295,6 +328,28 @@ if(!function_exists('friendlyDate')){
 	    }
 	}
 }
+function getDayPerMonth($year) {
+	   $arr = array(
+		   1 => 31,
+		   3 => 31,
+		   4 => 30,
+		   5 => 31,
+		   6 => 30,
+		   7 => 31,
+		   8 => 31,
+		   9 => 30,
+		   10 => 31,
+		   11 => 30,
+		   12 => 31
+	   );
+	   //闰年
+	   if(($year%4==0&&$year%100!=0) || ($year%400==0)) {
+		   $arr[2] = 29;
+	   } else {
+		   $arr[2] = 28;
+	   }
+	   return $arr;
+   }
 /**
  * 获取字符串的长度
  *
