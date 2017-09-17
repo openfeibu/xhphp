67989ef36dca5f6dd72a57f195d076fce3045891
wzj <wzj@23e8a065-ee1d-f14c-8958-478b19625c16>
2017-09-17 08:58:56 +0000

    git-svn-id: https://39.108.144.9:8443/svn/xhphp@789 23e8a065-ee1d-f14c-8958-478b19625c16

---
+++ projects/3.0.0/resources/views/emails/task.blade.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html lang="zh-CN">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  </head>
+<body>
+    有新的任务
+</body>
+</html>
