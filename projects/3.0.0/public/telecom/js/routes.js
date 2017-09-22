e79c161126dc79b928667384db64f9bdde097d3e
wzj <wzj@23e8a065-ee1d-f14c-8958-478b19625c16>
2017-09-22 16:34:33 +0000

    git-svn-id: https://39.108.144.9:8443/svn/xhphp@845 23e8a065-ee1d-f14c-8958-478b19625c16

--- projects/3.0.0/public/telecom/js/routes.js
+++ projects/3.0.0/public/telecom/js/routes.js
@@ -12,9 +12,11 @@ const routes = [
 // },
 //  { path: '/cancell', component: cancell ,meta: { msg: [{"value":"首页","path":"/"},{"value":"退款与售后","path":""}],index:"1-4" }
 // },
- { path: '/setting', component: setting ,meta: { msg: [{"value":"首页","path":"/"},{"value":"预约人数设置","path":""}],index:"2" }
+ { path: '/setting', component: setting ,meta: { msg: [{"value":"首页","path":"/home"},{"value":"预约人数设置","path":""}],index:"3" }
 },
- { path: '/order', component: table ,meta: { msg: [{"value":"首页","path":"/"},{"value":"订单预约","path":""}],index:"1" }
+ { path: '/order', component: table ,meta: { msg: [{"value":"首页","path":"/home"},{"value":"订单预约","path":""}],index:"2" }
+},
+ { path: '/home', component: home ,meta: { msg: [{"value":"首页","path":"/home"}],index:"1" }
 },
 ]
 const router = new VueRouter({
