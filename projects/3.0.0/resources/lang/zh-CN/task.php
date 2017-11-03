6e68e7d747134e8969fbb03b776308b8489e6bde
wzj <wzj@23e8a065-ee1d-f14c-8958-478b19625c16>
2017-11-03 03:01:04 +0000

    git-svn-id: https://39.108.144.9:8443/svn/xhphp@877 23e8a065-ee1d-f14c-8958-478b19625c16

--- projects/3.0.0/resources/lang/zh-CN/task.php
+++ projects/3.0.0/resources/lang/zh-CN/task.php
@@ -4,5 +4,5 @@ return [
     'task_be_accepted' => '您好，您发布的任务已被接入囊中',
     'task_be_finished' => '您好，接单人已完成你交付的任务，赶紧去看看。如果满意，请给任务结算吧。',
     'task_decription' => '共%s件商品，%skg',
-    'canteen_task_decription' => '共%s件商品，%s元',
+    'canteen_task_decription' => '%s件商品，共%s元',
 ];
