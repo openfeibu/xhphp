f10c793bb1aadc6cebc0c85ecdb3c0aff894b29f
wzj <wzj@23e8a065-ee1d-f14c-8958-478b19625c16>
2017-11-18 14:18:14 +0000

    git-svn-id: https://39.108.144.9:8443/svn/xhphp@910 23e8a065-ee1d-f14c-8958-478b19625c16

--- projects/3.0.0/app/Repositories/OrderInfoRepository.php
+++ projects/3.0.0/app/Repositories/OrderInfoRepository.php
@@ -33,7 +33,7 @@ class OrderInfoRepository
 				}
 			}
         	OrderGoods::insert($goodses);
-        	//Cart::where('shop_id',$order_info->shop_id)->where('uid',$order_info->uid)->delete();
+        	Cart::where('shop_id',$order_info->shop_id)->where('uid',$order_info->uid)->delete();
         	return $order_info;
         } catch (Exception $e) {
         	throw new \App\Exceptions\Custom\RequestFailedException('无法创建订单');
@@ -88,7 +88,7 @@ class OrderInfoRepository
 	}
 	public function getOrderInfo($order_id)
 	{
-		return OrderInfo::select(DB::raw('shop.shop_id,shop.shop_name,shop.shop_img,order_info.order_id,order_info.order_sn,order_info.pay_status,order_info.order_status,order_info.shipping_status,order_info.goods_amount,order_info.shipping_fee,order_info.total_fee,order_info.consignee,order_info.address,order_info.mobile,order_info.postscript,order_info.pay_time,order_info.user_coupon_id,order_info.shipping_adjust_fee,order_info.raise_fee,order_info.created_at'))
+		return OrderInfo::select(DB::raw('shop.shop_id,shop.shop_name,shop.shop_img,order_info.order_id,order_info.order_sn,order_info.pay_status,order_info.order_status,order_info.shipping_status,order_info.goods_amount,order_info.shipping_fee,order_info.total_fee,order_info.consignee,order_info.address,order_info.mobile,order_info.postscript,order_info.pay_time,order_info.user_coupon_id,order_info.goods_service_fee,order_info.shipping_adjust_fee,order_info.raise_fee,order_info.created_at'))
 							->leftJoin('shop', 'shop.shop_id', '=', 'order_info.shop_id')
 							->where('order_info.order_id',$order_id)
                           	->first();
