3376b8e5679e5938e63466cba4e97988f0684fc1
wzj <wzj@23e8a065-ee1d-f14c-8958-478b19625c16>
2017-11-18 13:29:25 +0000

    git-svn-id: https://39.108.144.9:8443/svn/xhphp@909 23e8a065-ee1d-f14c-8958-478b19625c16

--- projects/3.0.0/resources/lang/zh-CN/order_info.php
+++ projects/3.0.0/resources/lang/zh-CN/order_info.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'goods_service_fee_content' => '微信及支付宝所收支付费用' ,
+    'goods_service_fee_content' => '微信（支付宝）交易服务费' ,
     'goods_service_fee_detail' => '微信及支付宝所收支付费用,平台不收取任何费用',
 ];
